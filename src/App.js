96d700da25496822f75dc556c0dd9fd10d0a1d28
Sai Prasanna Korlakunta <skorlakunta-a@pplfirst.com>
2024-10-09 01:19:21 -0400

    new changes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>welcome to my world</h1>
+        <h2>om bheem bush</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
